967387a2a25f62fc0e72b23dd02ff80620a17e4b
varunjoshi27 <121236149+varunjoshi27@users.noreply.github.com>
2023-07-16 23:19:00 +0530

    Add files via upload
    This will print all the alphabet from A to Z.

---
+++ alphabet.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    char ch;
+     int i;
+     printf("The Alphabet's are \n");
+     for(i='A';i<='Z';i++){
+        
+        printf("%c\n",i);
+     }
+     return 0;
+}
