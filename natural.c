21231dc72489a98cd1520066875812f8c869b202
varunjoshi27 <121236149+varunjoshi27@users.noreply.github.com>
2023-07-16 23:06:29 +0530

    Add files via upload
    In this program user will input a number and then it print the sum of the numbers till that number.

---
+++ natural.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+
+    printf("Enter the number \n");
+    scanf("%d",&a);
+
+     printf("The sum of the natural number is \n");
+     printf("%d \n",a*(a-1));
+
+     return 0;
+}
