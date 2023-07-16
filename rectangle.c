c39fbeb2b1a3077ed312417628a8fe46d1e52d01
varunjoshi27 <121236149+varunjoshi27@users.noreply.github.com>
2023-07-16 23:03:51 +0530

    Add files via upload
    In this program user have to input length and width of the rectangle and than the area will be calculated.

---
+++ rectangle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    float l,b;
+    printf("Enter the value of length\n");
+    scanf("%f",&l);
+
+    printf("Enter the value of width\n");
+    scanf("%f",&b);
+
+    printf("The area of rectangle is %f",l*b);
+
+    return 0;
+}
